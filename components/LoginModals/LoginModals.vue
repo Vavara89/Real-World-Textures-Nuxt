ca7bcc47a93773d056d0fadd5474eb2ef63762ff
Lukas Hodac <hodac@pixelfield.cz>
2020-08-04 11:18:30 +0200

    Feat: Hlasky

---
+++ components/LoginModals/LoginModals.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <div></div>
+    <div >
+        <img src="" alt="" />
+        <h2>The email has already been taken.</h2>
+        <p>forgotten password?</p>
+        <a href="#">reset it</a>
+    </div>
+        <div>
+        <img src="" alt="" />
+        <h2>The captcha field is required.</h2>
+        <p>no materials for robots. sorry.</p>
+        <a href="#">try it again</a>
+    </div>
+        <div>
+        <img src="" alt="" />
+        <h2>Succesfully bookmarked</h2>
+        <p>You can download it from</p>
+        <a href="#">bookmark section</a>
+    </div>
+        <div>
+        <img src="" alt="" />
+        <h2>Something went wrong</h2>
+        <p>omg we're sorry. please, check it later.</p>
+        <a href="#">or get help</a>
+    </div>
+        <div>
+        <h2>Reset your password</h2>
+        <form>
+            <label>Your email adress</label>
+            <input type="text">
+            <button>confirm</button>
+        </form>
+    </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "LoginModals",
+  components: {
+  }
+};
+</script>
---
+++ pages/calls/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div >
+      <ul class="calls">
+        <li><a href="#">First</a></li>
+        <li><a href="#">Captcha failed</a></li>
+        <li><a href="#">Bookmarked</a></li>
+        <li><a href="#">Something wrong</a></li>
+        <li><a href="#">Reset password</a></li>
+    </ul>
+    <LoginModals />
+  </div>
+</template>
+
+<script>
+import LoginModals from "@/components/LoginModals/LoginModals";
+
+export default {
+  name: "Calls",
+  components: {
+    LoginModals,
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/pages/calls.scss";
+</style>
