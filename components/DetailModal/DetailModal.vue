350e401992f327fd4c67fbe73989ee8ebe7f272b
Lukas Hodac <hodac@pixelfield.cz>
2020-08-14 16:30:35 +0200

    Profile Status

--- components/DetailModal/DetailModal.vue
+++ components/DetailModal/DetailModal.vue
@@ -1,214 +1,222 @@
 <template>
-    <div class="detailModal" v-show="value">
-        <div class="container">
-            <div class="imageDetails">
-                <div class="imageDetails-header">
-                    <div>
-                        <a class="button" @click.prevent="close">
-                            <img src="@/assets/img/icon-cross.svg" style="margin:10px;" width="12" />
-                        </a>
-                    </div>
-                    <div class="imageDetails-options">
-                        <div class="download">
-                            <a href class="badge-circle color-bck-secondary--purple h4">
-                                <img src="@/assets/img/icon-download.svg" />
-                            </a>
-                        </div>
-                        <div v-show="texture.free" class="freeMark">
-                            <a href class="badge-rounded color-bck-secondary--green h4">Free</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="imageDetails-content">
-                    <div class="previewImg" v-show="texture.image">
-                        <img v-if="'image' in texture" :src="texture.image.url"/>
-                    </div>
-                    <div class="tutorial" v-show="texture.tutorial">
-                        <label class="badge-play h4">Tutorial</label>
-                    </div>
-                </div>
-                <div class="imageDetails-footer">
-                    <VueSlickCarousel ref="sliderNav" v-bind="navCarousel">
-                        <template #prevArrow="arrowOption">
-                            <div class="prev-slick">
-                                <a href="#" class="button">
-                                    <img src="@/assets/img/icon-arrow_left.svg"/>
-                                </a>
-                            </div>
-                        </template>
-                        <template #nextArrow="arrowOption">
-                            <div class="next-slick">
-                                <a href="#" class="button">
-                                    <img src="@/assets/img/icon-arrow_left.svg"/>
-                                </a>
-                            </div>
-                        </template>
-                        <div class="imageItem">
-                            <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class />
-                        </div>
-                        <div class="imageItem">
-                            <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class />
-                        </div>
-                        <div class="imageItem">
-                            <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class />
-                        </div>
-                        <div class="imageItem">
-                            <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class />
-                        </div>
-                        <div class="imageItem">
-                            <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class />
-                        </div>
-                        <div class="imageItem">
-                            <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class />
-                        </div>
-                        <div class="imageItem">
-                            <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class />
-                        </div>
-                        <div class="imageItem">
-                            <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class />
-                        </div>
-                    </VueSlickCarousel>
-                </div>
+  <div v-show="value" class="detailModal">
+    <div class="container">
+      <div class="imageDetails">
+        <div class="imageDetails-header">
+          <div>
+            <a class="button" @click.prevent="close">
+              <img src="@/assets/img/icon-cross.svg" style="margin:10px;" width="12">
+            </a>
+          </div>
+          <div class="imageDetails-options">
+            <div class="download">
+              <a href class="badge-circle color-bck-secondary--purple h4">
+                <img src="@/assets/img/icon-download.svg">
+              </a>
             </div>
-            <div class="details">
-                <div class="title">
-                    <h4 class="name">{{ texture.title }}</h4>
-                </div>
-                <div class="credits-bookmark">
-                    <div class="credits h3" v-if="texture.credits">
-                        <label>{{ texture.credits }}</label>
-                        credits
-                    </div>
-                    <div class="bookmark">
-                        <img src="@/assets/img/icon-bookmark-1.svg" />
-                    </div>
-                </div>
-                <div class="options">
-                    <div class="option-item resolution">
-                        <div class="label">
-                            <label class="h3">Resolution:</label>
-                        </div>
-                        <div class="option">
-                            <Dropdown :options="options"/>
-                        </div>
-                    </div>
-                    <div class="option-item dimension">
-                        <div class="label">
-                            <label class="h3">Texture<br/>Dimensions:</label>
-                        </div>
-                        <div class="option">
-                            <label class="text">yyy X yyy cm</label>
-                        </div>
-                    </div>
-                    <div class="option-item">
-                        <div class="label">
-                            <label class="h3">Consisting of:</label>
-                        </div>
-                        <div class="option">
-                            <ul class="consisting-list">
-                                <li class="text" v-for="(item, index) in texture.consist" :key="'consist-' + index">{{ item }}</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="description">
-                    <label class="h3">Description:</label>
-                    <p class="text">{{ texture.description }}</p>
-                </div>
-                <div class="brand">
-                    <div class="label">
-                        <label class="h3">Brand:</label>
-                    </div>
-                    <div class="option" v-if="texture.brand">
-                        <label class="brand">{{ texture.brand.name }}</label>
-                        <a class="brand-link" :href="texture.brand.link">{{ texture.brand.link }}</a>
-                    </div>
-                </div>
-                <div class="download">
-                    <button v-show="!processing" @click="process" class="button-primary">Download for {{ texture.credits }} credits</button>   
-                    <button v-show="processing" class="button-primary button-primary--blue">
-                        <img src="@/assets/img/icon-processing-button.svg"/>
-                        Processing... Continue Browsing
-                    </button>
-                </div>
+            <div v-show="texture.free" class="freeMark">
+              <a href class="badge-rounded color-bck-secondary--green h4">Free</a>
             </div>
+          </div>
         </div>
+        <div class="imageDetails-content">
+          <div v-show="texture.image" class="previewImg">
+            <img v-if="'image' in texture" :src="texture.image.url">
+          </div>
+          <div v-show="texture.tutorial" class="tutorial">
+            <label class="badge-play h4">Tutorial</label>
+          </div>
+        </div>
+        <div class="imageDetails-footer">
+          <VueSlickCarousel ref="sliderNav" v-bind="navCarousel">
+            <template #prevArrow="arrowOption">
+              <div class="prev-slick">
+                <a href="#" class="button">
+                  <img src="@/assets/img/icon-arrow_left.svg">
+                </a>
+              </div>
+            </template>
+            <template #nextArrow="arrowOption">
+              <div class="next-slick">
+                <a href="#" class="button">
+                  <img src="@/assets/img/icon-arrow_left.svg">
+                </a>
+              </div>
+            </template>
+            <div class="imageItem">
+              <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class>
+            </div>
+            <div class="imageItem">
+              <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class>
+            </div>
+            <div class="imageItem">
+              <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class>
+            </div>
+            <div class="imageItem">
+              <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class>
+            </div>
+            <div class="imageItem">
+              <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class>
+            </div>
+            <div class="imageItem">
+              <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class>
+            </div>
+            <div class="imageItem">
+              <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class>
+            </div>
+            <div class="imageItem">
+              <img v-if="'image' in texture" :src="texture.image.url" :alt="texture.image.alt" class>
+            </div>
+          </VueSlickCarousel>
+        </div>
+      </div>
+      <div class="details">
+        <div class="title">
+          <h4 class="name">
+            {{ texture.title }}
+          </h4>
+        </div>
+        <div class="credits-bookmark">
+          <div v-if="texture.credits" class="credits h3">
+            <label>{{ texture.credits }}</label>
+            credits
+          </div>
+          <div class="bookmark">
+            <img src="@/assets/img/icon-bookmark-1.svg">
+          </div>
+        </div>
+        <div class="options">
+          <div class="option-item resolution">
+            <div class="label">
+              <label class="h3">Resolution:</label>
+            </div>
+            <div class="option">
+              <Dropdown :options="options" />
+            </div>
+          </div>
+          <div class="option-item dimension">
+            <div class="label">
+              <label class="h3">Texture<br>Dimensions:</label>
+            </div>
+            <div class="option">
+              <label class="text">yyy X yyy cm</label>
+            </div>
+          </div>
+          <div class="option-item">
+            <div class="label">
+              <label class="h3">Consisting of:</label>
+            </div>
+            <div class="option">
+              <ul class="consisting-list">
+                <li v-for="(item, index) in texture.consist" :key="'consist-' + index" class="text">
+                  {{ item }}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="description">
+          <label class="h3">Description:</label>
+          <p class="text">
+            {{ texture.description }}
+          </p>
+        </div>
+        <div class="brand">
+          <div class="label">
+            <label class="h3">Brand:</label>
+          </div>
+          <div v-if="texture.brand" class="option">
+            <label class="brand">{{ texture.brand.name }}</label>
+            <a class="brand-link" :href="texture.brand.link">{{ texture.brand.link }}</a>
+          </div>
+        </div>
+        <div class="download">
+          <button v-show="!processing" class="button-primary" @click="process">
+            Download for {{ texture.credits }} credits
+          </button>
+          <button v-show="processing" class="button-primary button-primary--blue">
+            <img src="@/assets/img/icon-processing-button.svg">
+            Processing... Continue Browsing
+          </button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-import VueSlickCarousel from "vue-slick-carousel";
-import "vue-slick-carousel/dist/vue-slick-carousel.css";
-import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
-import Dropdown from "@/components/Sidebar/Dropdown";
+import VueSlickCarousel from 'vue-slick-carousel';
+import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+import Dropdown from '@/components/Sidebar/Dropdown';
 
 export default {
-    name: "DetailModal",
-    components: {
-        VueSlickCarousel,
-        Dropdown
+  name: 'DetailModal',
+  components: {
+    VueSlickCarousel,
+    Dropdown
+  },
+
+  props: {
+    texture: {
+      type: Object
     },
-
-    props: {
-        texture: {
-            type: Object,
-        },
-        value: {
-            required: true,
-        }
-    },
-
-    data() {
-        return {
-            navCarousel: {
-                dots: false,
-                infinite: false,
-                centerMode: false,
-                centerPadding: "1px",
-                variableWidth: false,
-                slidesToScroll: 1,
-                slidesToShow: 5,
-                asNavFor: {},
-                initialSlide: 0,
-                useTranform: false,
-            },
-            options: [
-                {
-                    value: "Choose resolutions"
-                },
-                {
-                    value: "1920x1920 px"
-                },
-                {
-                    value: "2560x2560 px (2k)"
-                },
-                {
-                    value: "4096x4096 px (4k)"
-                },
-                {
-                    value: "7680x7680 px (8k)"
-                },
-                {
-                    value: "10240x10240 px (10k)"
-                },
-            ],
-            processing: false,
-        }
-    },
-
-    methods: {
-        close() {
-            this.$emit("input", !this.value)
-        },
-
-        process() {
-            this.processing = !this.processing;
-        }
-    },
-
-    mounted() {
-        this.navCarousel.asNavFor = this.$refs.sliderMain;
+    value: {
+      required: true
     }
-}
+  },
+
+  data () {
+    return {
+      navCarousel: {
+        dots: false,
+        infinite: false,
+        centerMode: false,
+        centerPadding: '1px',
+        variableWidth: false,
+        slidesToScroll: 1,
+        slidesToShow: 5,
+        asNavFor: {},
+        initialSlide: 0,
+        useTranform: false
+      },
+      options: [
+        {
+          value: 'Choose resolutions'
+        },
+        {
+          value: '1920x1920 px'
+        },
+        {
+          value: '2560x2560 px (2k)'
+        },
+        {
+          value: '4096x4096 px (4k)'
+        },
+        {
+          value: '7680x7680 px (8k)'
+        },
+        {
+          value: '10240x10240 px (10k)'
+        }
+      ],
+      processing: false
+    };
+  },
+
+  mounted () {
+    this.navCarousel.asNavFor = this.$refs.sliderMain;
+  },
+
+  methods: {
+    close () {
+      this.$emit('input', !this.value);
+    },
+
+    process () {
+      this.processing = !this.processing;
+    }
+  }
+};
 </script>
 <style lang="scss">
     @import "@/assets/scss/components/_detailModal.scss";
--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -4,9 +4,15 @@
       <div class="rows">
         <div class="cols">
           <div class="inner">
-            <p class="sub_heading">Let's welcome our</p>
-            <p class="heading">New partners</p>
-            <p class="perx">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed tortor a felis rhoncus pretium ac sit amet nibh. Aenean ac malesuada quam, et tempor magna.</p>
+            <p class="sub_heading">
+              Let's welcome our
+            </p>
+            <p class="heading">
+              New partners
+            </p>
+            <p class="perx">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed tortor a felis rhoncus pretium ac sit amet nibh. Aenean ac malesuada quam, et tempor magna.
+            </p>
             <Button
               link="#"
               text="Exploare all brands"
@@ -52,9 +58,15 @@
       <div class="rows">
         <div class="cols">
           <div class="inner">
-            <p class="sub_heading">Have you seen</p>
-            <p class="heading">Hot news?</p>
-            <p class="perx">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed tortor a felis rhoncus pretium ac sit amet nibh. Aenean ac malesuada quam, et tempor magna.</p>
+            <p class="sub_heading">
+              Have you seen
+            </p>
+            <p class="heading">
+              Hot news?
+            </p>
+            <p class="perx">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed tortor a felis rhoncus pretium ac sit amet nibh. Aenean ac malesuada quam, et tempor magna.
+            </p>
             <Button
               link="#"
               text="Join our community on FB"
@@ -64,13 +76,52 @@
           </div>
         </div>
         <div class="cols news">
-            <section class="services view-bottom">
-              <div class="container container--box">
-                <!-- <SectionTitle :title="title" :perex="perex" :button="button" :isCentered="true" /> -->
-                <CardGrid :cards="cards" :is-more="true" :is-tutorial="true" />
-              </div>
-            </section>
+          <section class="services view-bottom">
+            <div class="container container--box">
+              <!-- <SectionTitle :title="title" :perex="perex" :button="button" :isCentered="true" /> -->
+              <CardGrid :cards="cards" :is-more="true" :is-tutorial="true" />
+            </div>
+          </section>
         </div>
+      </div>
+    </section>
+    <section>
+      <div class="imageDetails-footer last">
+        <SectionTitle :subtitle="subtitle_textures" :title="title_textures" :is-centered="true" />
+        <VueSlickCarousel ref="sliderNav" v-bind="navCarousel">
+          <template #prevArrow="arrowOption">
+            <div class="prev-slick">
+              <span class="button superb">
+                <img src="@/assets/img/icon-sliderarr.svg">
+              </span>
+            </div>
+          </template>
+          <template #nextArrow="arrowOption">
+            <div class="next-slick">
+              <span class="button superb">
+                <img src="@/assets/img/icon-sliderarr.svg">
+              </span>
+            </div>
+          </template>
+          <div class="imageItem">
+            <img src="@/assets/img/textures/coral.svg">
+          </div>
+          <div class="imageItem">
+            <img src="@/assets/img/textures/red.svg">
+          </div>
+          <div class="imageItem">
+            <img src="@/assets/img/textures/blue.svg">
+          </div>
+          <div class="imageItem">
+            <img src="@/assets/img/textures/sand.svg">
+          </div>
+          <div class="imageItem">
+            <img src="@/assets/img/textures/brown.svg">
+          </div>
+          <div class="imageItem">
+            <img src="@/assets/img/textures/blue.svg">
+          </div>
+        </VueSlickCarousel>
       </div>
     </section>
     <SectionSubscribe />
@@ -79,11 +130,15 @@
 </template>
 
 <script>
+import VueSlickCarousel from 'vue-slick-carousel';
 import SectionTitle from '@/components/SectionParts/SectionTitle';
 import CardGrid from '@/components/Cards/CardGrid';
 import SectionSubscribe from '@/components/Sections/SectionSubscribe';
 import SectionFaq from '@/components/Sections/SectionFaq';
 import Button from '@/components/Button';
+
+import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
 
 export default {
   name: 'Tutorials',
@@ -92,12 +147,15 @@ export default {
     CardGrid,
     SectionSubscribe,
     SectionFaq,
-    Button
+    Button,
+    VueSlickCarousel
   },
   data () {
     return {
       subtitle: 'Services',
       title: 'Tutorials',
+      subtitle_textures: 'Latest releases of',
+      title_textures: 'Tutorials',
       perex:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sed tortor a felis rhoncus pretium ac sit amet nibh. Aenean ac malesuada quam, et tempor magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       button: 'Join our community on FB',
@@ -148,8 +206,67 @@ export default {
             'Scann and postprocess your material or model, provide it through our website and get to know architects about your company and  its products.',
           link: '#'
         }
-      ]
+      ],
+      navCarousel: {
+        dots: false,
+        infinite: false,
+        centerMode: false,
+        centerPadding: '1px',
+        variableWidth: false,
+        slidesToScroll: 1,
+        slidesToShow: this.showWindow ? this.showCount() : 0,
+        asNavFor: {},
+        initialSlide: 0,
+        useTranform: false
+      },
+      options: [
+        {
+          value: 'Choose resolutions'
+        },
+        {
+          value: '1920x1920 px'
+        },
+        {
+          value: '2560x2560 px (2k)'
+        },
+        {
+          value: '4096x4096 px (4k)'
+        },
+        {
+          value: '7680x7680 px (8k)'
+        },
+        {
+          value: '10240x10240 px (10k)'
+        }
+      ],
+      processing: false,
+      showWindow: false
     };
+  },
+  mounted () {
+    this.navCarousel.asNavFor = this.$refs.sliderMain;
+    this.$nextTick(function () {
+      this.onResize();
+    });
+    window.addEventListener('resize', this.onResize);
+  },
+  destroyed () {
+    window.removeEventListener('resize', this.onResize);
+  },
+  methods: {
+    onResize () {
+      this.showCount();
+    },
+    showCount () {
+      this.showWindow = true;
+
+      if (window.innerWidth < 950) {
+        console.log(2);
+        return 3;
+      } else {
+        return 5;
+      }
+    }
   }
 };
 </script>
@@ -157,4 +274,5 @@ export default {
 <style lang="scss" scoped>
 @import "@/assets/scss/pages/tutorials.scss";
 @import "@/assets/scss/pages/profile.scss";
+@import "@/assets/scss/components/_detailModal.scss";
 </style>
