e0f6e3e3c4030a3d778f0acaa5bc42a075dd8100
Ulianae <dmitrieva@pixelfield.cz>
2020-12-14 15:57:38 +0100

    fix: 17222 Pricing

--- components/Cards/PriceCard.vue
+++ components/Cards/PriceCard.vue
@@ -1,25 +1,42 @@
 <template>
   <div class="pricecard__block">
-    <div class="pricecard__content">
-      <p class="pricecard__title">
+    <div class="pricecard__header">
+      <div class="pricecard__title">
         {{ card.title }}
-      </p>
-      <p class="pricecard__subtitle">
-        {{ card.subtitle }}
-      </p>
-      <p v-if="duration === 'year'" class="pricecard__price">
-        {{ card.currency }}{{ card.priceYear }}
-      </p>
-      <p v-if="duration !== 'year'" class="pricecard__price">
-        {{ card.currency }}{{ card.price }}
-      </p>
-      <p v-if="duration === 'year'" class="pricecard__desc">
-        {{ card.desc_year }}
-      </p>
-      <p v-if="duration !== 'year'" class="pricecard__desc">
-        {{ card.desc_month }}
-      </p>
-        <Button  @click="subscribe" :link="'javascript:void(0);'" :text="card.buttontext" type="primary" width="wide" />
+      </div>
+      <div v-if="duration === 'year'" class="pricecard__price">
+        {{ card.currency }}{{ card.price_year }}
+      </div>
+      <div v-else class="pricecard__price">
+        {{ card.currency }}{{ card.price_month }}
+      </div>
+    </div>
+    <div class="pricecard__body">
+      <div>
+        <div class="pricecard__descr">
+          <div v-if="duration === 'year'" class="pricecard__descr-title">
+            {{ card.descr_title_year }}
+          </div>
+          <div v-else class="pricecard__descr-title">
+            {{ card.descr_title_month }}
+          </div>
+          <div class="pricecard__descr-subtitle">
+            {{ card.descr_subtitle }}
+          </div>
+        </div>
+        <div>
+          <p class="pricecard__text">
+            {{ card.descr_text1 }}
+          </p>
+          <p class="pricecard__text">
+            {{ card.descr_text2 }}
+          </p>
+          <p class="pricecard__text">
+            {{ card.descr_text3 }}
+          </p>
+        </div>
+      </div>
+      <Button :link="'javascript:void(0);'" :text="card.buttontext" type="primary" width="wide" @click="subscribe" />
     </div>
   </div>
 </template>
@@ -42,28 +59,28 @@ export default {
       default: ''
     }
   },
-  methods:{
-    subscribe(e){
-      if(!this.user){
-        this.$router.replace('/login')
-      }else{
-        this.$router.replace('/profile/dashboard?payment=true')
-      }
-    }
-  },
-  computed:{
-    user(){
+  computed: {
+    user () {
       return this.$auth.user ? this.$auth.user.user : null;
     },
-    payment(){
-      if(this.$auth.user && this.$auth.user.user.payment){
+    payment () {
+      if (this.$auth.user && this.$auth.user.user.payment) {
         const payment = this.$auth.user.user.payment;
-        if(payment && payment.last4){
+        if (payment && payment.last4) {
           return payment;
         }
       }
       return false;
-    },
+    }
+  },
+  methods: {
+    subscribe (e) {
+      if (!this.user) {
+        this.$router.replace('/login');
+      } else {
+        this.$router.replace('/profile/dashboard?payment=true');
+      }
+    }
   }
 };
 </script>
--- components/Sections/SectionSubscribe.vue
+++ components/Sections/SectionSubscribe.vue
@@ -41,58 +41,100 @@ export default {
       duration: ''
     };
   },
-  computed:{
-    cards(){
+  computed: {
+    cards () {
       const prices = this.$store.getters.prices;
-      if(!prices.length){
+      if (!prices.length) {
         return [];
       }
       const products = {
-        'pro':prices.filter(item => item.stripe_product.is_pro === true),
-        'freelancer': prices.filter(item => item.stripe_product.is_pro === false)
+        pro: prices.filter(item => item.stripe_product.is_pro === true),
+        freelancer: prices.filter(item => item.stripe_product.is_pro === false)
       };
-      if(products.pro.length && products.freelancer.length){
+      if (products.pro.length && products.freelancer.length) {
         const titles = {
-          'pro': products.pro[0].stripe_product.name,
-          'freelancer': products.freelancer[0].stripe_product.name,
+          pro: products.pro[0].stripe_product.name,
+          freelancer: products.freelancer[0].stripe_product.name
         };
         const price_year = {
-            'pro':products.pro.filter(item => item.is_year === true)[0],
-            'freelancer':products.freelancer.filter(item => item.is_year === true)[0],
+          pro: products.pro.filter(item => item.is_year === true)[0],
+          freelancer: products.freelancer.filter(item => item.is_year === true)[0]
         };
         const price_month = {
-          'pro':products.pro.filter(item => item.is_year === false)[0],
-          'freelancer':products.freelancer.filter(item => item.is_year === false)[0],
+          pro: products.pro.filter(item => item.is_year === false)[0],
+          freelancer: products.freelancer.filter(item => item.is_year === false)[0]
         };
 
         return [
           {
-            title: titles.pro,
-            subtitle: 'per month/person',
+            title: 'Free',
+            descr_title_month: `200+ freebies`,
+            descr_title_year: `400+ freebies`,
+            descr_subtitle: '',
+            descr_text1: 'Try it and enjoy our freebies',
+            descr_text2: 'Lorem ipsum',
+            descr_text3: 'Dolor sit amet',
             currency: '$',
-            price: price_month.pro.amount,
-            priceYear: parseInt(price_year.pro.amount)/12,
-            desc: `${price_month.pro.credits} credits/month`,
-            desc_month: `${price_month.pro.credits} credits/month`,
-            desc_year: `${parseInt(price_year.pro.credits)/12} credits/month`,
-            buttontext: `Get ${titles.pro}`,
+            price_month: '0',
+            price_year: '0',
+            buttontext: 'Try for free',
             buttonlink: '/'
           },
           {
-            title: titles.freelancer,
-            subtitle: 'per month/seat',
+            title: 'Freelance',
+            descr_title_month: `${price_month.freelancer.credits} credits`,
+            descr_title_year: `${price_year.freelancer.credits} credits`,
+            descr_subtitle: 'every month',
+            descr_text1: 'For one individual with revenue  under $100K',
+            descr_text2: 'Months of Credit Rollover With Active Subscription',
+            descr_text3: 'For 200 credits you get aprox. 45 Surfaces, or 22 3D Models, or 90 other asset types',
             currency: '$',
-            price: price_month.freelancer.amount,
-            priceYear: parseInt(price_year.freelancer.amount)/12,
-            desc: `${price_month.freelancer.credits} credits/month`,
-            desc_month: `${price_month.freelancer.credits} credits/month`,
-            desc_year: `${parseInt(price_year.freelancer.credits)/12} credits/month`,
-            buttontext: `Get ${titles.freelancer}`,
+            price_month: `${price_month.freelancer.amount}`,
+            price_year: `${parseInt(price_year.freelancer.amount) / 12}`,
+            buttontext: 'Get freelance',
+            buttonlink: '/'
+          },
+          {
+            title: 'Professional',
+            descr_title_month: `${price_month.pro.credits} credits`,
+            descr_title_year: `${price_year.pro.credits} credits`,
+            descr_subtitle: 'every month',
+            descr_text1: 'For one seat with company revenue above $100K',
+            descr_text2: 'Months of Credit Rollover With Active Subscription',
+            descr_text3: 'For 700 credits you get aprox. 100 Surfaces, or 50 3D Models, or 150 other asset types',
+            currency: '$',
+            price_month: `${price_month.pro.amount}`,
+            price_year: `${parseInt(price_year.pro.amount) / 12}`,
+            buttontext: 'Get professional',
             buttonlink: '/'
           }
+          // {
+          //   title: titles.freelancer,
+          //   subtitle: 'per month/seat',
+          //   currency: '$',
+          //   price: price_month.freelancer.amount,
+          //   priceYear: parseInt(price_year.freelancer.amount) / 12,
+          //   desc: `${price_month.freelancer.credits} credits/month`,
+          //   desc_month: `${price_month.freelancer.credits} credits/month`,
+          //   desc_year: `${parseInt(price_year.freelancer.credits) / 12} credits/month`,
+          //   buttontext: `Get ${titles.freelancer}`,
+          //   buttonlink: '/'
+          // },
+          // {
+          //   title: titles.freelancer,
+          //   subtitle: 'per month/seat',
+          //   currency: '$',
+          //   price: price_month.freelancer.amount,
+          //   priceYear: parseInt(price_year.freelancer.amount) / 12,
+          //   desc: `${price_month.freelancer.credits} credits/month`,
+          //   desc_month: `${price_month.freelancer.credits} credits/month`,
+          //   desc_year: `${parseInt(price_year.freelancer.credits) / 12} credits/month`,
+          //   buttontext: `Get ${titles.freelancer}`,
+          //   buttonlink: '/'
+          // }
         ];
       }
-      return  [];
+      return [];
     }
   },
   methods: {
@@ -104,7 +146,7 @@ export default {
         this.duration = duration;
       }
     },
-    getLink(){
+    getLink () {
     }
   }
 };
